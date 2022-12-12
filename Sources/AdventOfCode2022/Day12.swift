76cf1a4c72a57cf994133a801d51af80478bf759
James Linnell <james.n.linnell@gmail.com>
2022-12-11 23:45:46 -0800

    Day 12 cleaned

--- Sources/AdventOfCode2022/Day12.swift
+++ Sources/AdventOfCode2022/Day12.swift
@@ -37,7 +37,7 @@ public struct Day12: Challenge {
 		}
 	}
 	
-	public func solvePart1(input: String) -> String {
+	func solve(input: String, breakCondPart2: Bool) -> String {
 		let cells = input
 			.split(separator: "\n")
 			.map({ [Cell]($0.map({ Cell(value: $0) })) })
@@ -50,36 +50,45 @@ public struct Day12: Challenge {
 		let endCoord = Coord(row: endRow, col: endCol)
 		
 		let dirs: [(Int, Int)] = [(0, -1), (0, 1), (1, 0), (-1, 0)]
-		var coordsToProcess: [Coord] = [endCoord]
-		outer: while !coordsToProcess.isEmpty {
+		var coordsToProcess: [Coord] = []
+		
+		cells[endCoord.row][endCoord.col].stepsToEnd = 0
+		coordsToProcess.append(contentsOf: dirs
+			.map({ Coord(row: endCoord.row + $0.0, col: endCoord.col + $0.1) })
+			.filter({ 0 <= $0.row && $0.row < cells.count && 0 <= $0.col && $0.col < cells[0].count }))
+		
+		while !coordsToProcess.isEmpty {
 			let coord = coordsToProcess.first!
 			let dirCoords: [Coord] = dirs
 				.map({ Coord(row: coord.row + $0.0, col: coord.col + $0.1) })
 				.filter({ 0 <= $0.row && $0.row < cells.count && 0 <= $0.col && $0.col < cells[0].count })
-			if coord == endCoord {
-				cells[coord.row][coord.col].stepsToEnd = 0
+			
+			let cell = cells[coord.row][coord.col]
+			let neighborsInOrder: [(Coord, Cell)] = dirCoords
+				.map({ ($0, cells[$0.row][$0.col]) })
+				.filter({ $0.1.stepsToEnd != nil && (cell.value == $0.1.value || cell.value + 1 == $0.1.value || cell.value > $0.1.value) })
+				.sorted(by: { $0.1.stepsToEnd ?? 0 < $1.1.stepsToEnd ?? 0 })
+			if let best = neighborsInOrder.first {
+				cell.stepsToEnd = best.1.stepsToEnd! + 1
 				_ = coordsToProcess.removeFirst()
+				coordsToProcess.removeAll(where: { $0 == coord })
 				coordsToProcess.append(contentsOf: dirCoords.filter({ cells[$0.row][$0.col].stepsToEnd == nil }))
-			} else {
-				let cell = cells[coord.row][coord.col]
-				let neighborsInOrder: [(Coord, Cell)] = dirCoords
-					.map({ ($0, cells[$0.row][$0.col]) })
-					.filter({ $0.1.stepsToEnd != nil && (cell.value == $0.1.value || cell.value + 1 == $0.1.value || cell.value > $0.1.value) })
-					.sorted(by: { $0.1.stepsToEnd ?? 0 < $1.1.stepsToEnd ?? 0 })
-				if let best = neighborsInOrder.first {
-					cell.stepsToEnd = best.1.stepsToEnd! + 1
-					_ = coordsToProcess.removeFirst()
-					coordsToProcess.removeAll(where: { $0 == coord })
-					coordsToProcess.append(contentsOf: dirCoords.filter({ cells[$0.row][$0.col].stepsToEnd == nil }))
-					
-					if cell.isStart {
-						break outer
+				
+				if breakCondPart2 {
+					if cell.ascii == "a" {
+						return String(cell.stepsToEnd!)
 					} else {
-						continue outer
+						continue
+					}
+				} else {
+					if cell.isStart {
+						break
+					} else {
+						continue
 					}
 				}
-				coordsToProcess.append(coordsToProcess.removeFirst()) // No matches, pop and requeue to end
 			}
+			coordsToProcess.append(coordsToProcess.removeFirst()) // No matches, pop and requeue to end
 		}
 		
 //		print(cells.map({ String($0.map({ String(format: "%03d", $0.stepsToEnd ?? -1) }).joined(separator: " ")) }).joined(separator: "\n"))
@@ -87,52 +96,12 @@ public struct Day12: Challenge {
 		return "\(cells[startCoord.row][startCoord.col].stepsToEnd ?? 0)"
 	}
 	
+	public func solvePart1(input: String) -> String {
+		return solve(input: input, breakCondPart2: false)
+	}
+	
 	public func solvePart2(input: String) -> String {
-		let cells = input
-			.split(separator: "\n")
-			.map({ [Cell]($0.map({ Cell(value: $0) })) })
-		
-		let startRow = cells.enumerated().first(where: { $0.1.contains(where: { $0.isStart }) })!.0
-		let startCol = cells[startRow].enumerated().first(where: { $0.1.isStart })!.0
-		let startCoord = Coord(row: startRow, col: startCol)
-		let endRow = cells.enumerated().first(where: { $0.1.contains(where: { $0.isEnd }) })!.0
-		let endCol = cells[endRow].enumerated().first(where: { $0.1.isEnd })!.0
-		let endCoord = Coord(row: endRow, col: endCol)
-		
-		let dirs: [(Int, Int)] = [(0, -1), (0, 1), (1, 0), (-1, 0)]
-		var coordsToProcess: [Coord] = [endCoord]
-		outer: while !coordsToProcess.isEmpty {
-			let coord = coordsToProcess.first!
-			let dirCoords: [Coord] = dirs
-				.map({ Coord(row: coord.row + $0.0, col: coord.col + $0.1) })
-				.filter({ 0 <= $0.row && $0.row < cells.count && 0 <= $0.col && $0.col < cells[0].count })
-			if coord == endCoord {
-				cells[coord.row][coord.col].stepsToEnd = 0
-				_ = coordsToProcess.removeFirst()
-				coordsToProcess.append(contentsOf: dirCoords.filter({ cells[$0.row][$0.col].stepsToEnd == nil }))
-			} else {
-				let cell = cells[coord.row][coord.col]
-				let neighborsInOrder: [(Coord, Cell)] = dirCoords
-					.map({ ($0, cells[$0.row][$0.col]) })
-					.filter({ $0.1.stepsToEnd != nil && (cell.value == $0.1.value || cell.value + 1 == $0.1.value || cell.value > $0.1.value) })
-					.sorted(by: { $0.1.stepsToEnd ?? 0 < $1.1.stepsToEnd ?? 0 })
-				if let best = neighborsInOrder.first {
-					cell.stepsToEnd = best.1.stepsToEnd! + 1
-					_ = coordsToProcess.removeFirst()
-					coordsToProcess.removeAll(where: { $0 == coord })
-					coordsToProcess.append(contentsOf: dirCoords.filter({ cells[$0.row][$0.col].stepsToEnd == nil }))
-					
-					if cell.ascii == "a" {
-						return String(cell.stepsToEnd!)
-					} else {
-						continue outer
-					}
-				}
-				coordsToProcess.append(coordsToProcess.removeFirst()) // No matches, pop and requeue to end
-			}
-		}
-		
-		return "error"
+		return solve(input: input, breakCondPart2: true)
 	}
 	
 }
