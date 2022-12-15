4460e7d2b1f0620a888c04b19ef9ae4c83ae4760
James Linnell <james.n.linnell@gmail.com>
2022-12-14 23:14:38 -0800

    Day 15 cleaned

--- Sources/AdventOfCode2022/Day15.swift
+++ Sources/AdventOfCode2022/Day15.swift
@@ -31,25 +31,6 @@ public struct Day15: Challenge {
 				return (Coord(x: Int(sensorSplit[0])!, y: Int(sensorSplit[1])!), Coord(x: Int(beaconSplit[0])!, y: Int(beaconSplit[1])!))
 			})
 		
-		var board: [Coord: Cell] = [:]
-		for coord in coords {
-			board[coord.0] = .sensor
-			board[coord.1] = .beacon
-		}
-		
-		let minX = coords.map({ [$0.0.x, $0.1.x] }).flatMap({ $0 }).min()!
-		let maxX = coords.map({ [$0.0.x, $0.1.x] }).flatMap({ $0 }).max()!
-		let minY = coords.map({ [$0.0.y, $0.1.y] }).flatMap({ $0 }).min()!
-		let maxY = coords.map({ [$0.0.y, $0.1.y] }).flatMap({ $0 }).max()!
-		
-//		for y in minY...maxY {
-//			for x in minX...maxX {
-//				print((board[Coord(x: x, y: y)] ?? .unknown).rawValue, terminator: "")
-//			}
-//			print()
-//		}
-//		print()
-		
 		let targetY = self.targetY
 		var targetYLinePairs: [(Coord, Coord)] = []
 		for coord in coords {
@@ -64,56 +45,6 @@ public struct Day15: Challenge {
 			let right = Coord(x: sensorCoord.x + (1 * (manhattanDistance - distanceToTargetY)), y: targetY)
 			targetYLinePairs.append((left, right))
 		}
-		
-		/*
-		for coord in coords {
-			let sensorCoord = coord.0
-			let beaconCoord = coord.1
-			
-			let manhattanDistance = abs(sensorCoord.x - beaconCoord.x) + abs(sensorCoord.y - beaconCoord.y)
-			let dirs = [Coord(x: 0, y: 1), Coord(x: 0, y: -1), Coord(x: 1, y: 0), Coord(x: -1, y: 0)]
-			for (i, radius) in (1...manhattanDistance).reversed().enumerated() {
-				let cells = dirs
-					.map({ Coord(x: sensorCoord.x + $0.x * radius, y: sensorCoord.y + $0.y * radius) })
-					+ (0...i).map({ Coord(x: sensorCoord.x + $0, y: sensorCoord.y + -1 * radius) })
-					+ (0...i).map({ Coord(x: sensorCoord.x - $0, y: sensorCoord.y + 1 * radius) })
-					+ (0...i).map({ Coord(x: sensorCoord.x + -1 * radius, y: sensorCoord.y - $0) })
-					+ (0...i).map({ Coord(x: sensorCoord.x + 1 * radius, y: sensorCoord.y + $0) })
-				for cell in cells {
-					board[cell] = .empty
-				}
-			}
-			
-//			for y in minY...maxY {
-//				for x in minX...maxX {
-//					print((board[Coord(x: x, y: y)] ?? .unknown).rawValue, terminator: "")
-//				}
-//				print()
-//			}
-//			print()
-		}
-		*/
-		
-//		for coord in coords {
-//			board[coord.0] = .sensor
-//			board[coord.1] = .beacon
-//		}
-		
-//		for y in minY...maxY {
-//			for x in minX...maxX {
-//				print((board[Coord(x: x, y: y)] ?? .unknown).rawValue, terminator: "")
-//			}
-//			print()
-//		}
-//		print()
-		
-//		let newMinX = coords.map({ [$0.0.x, $0.1.x] }).flatMap({ $0 }).min()!
-//		let newMaxX = coords.map({ [$0.0.x, $0.1.x] }).flatMap({ $0 }).max()!
-//		let newMinY = coords.map({ [$0.0.y, $0.1.y] }).flatMap({ $0 }).min()!
-//		let newMaxY = coords.map({ [$0.0.y, $0.1.y] }).flatMap({ $0 }).max()!
-		
-//		let emptyOnY10 = (newMinX...newMaxX).map({ Coord(x: $0, y: 10) }).filter({ board[$0] == .empty }).count
-//		let emptyOnY2000000 = (newMinX...newMaxX).map({ Coord(x: $0, y: 2000000) }).filter({ board[$0] == .empty }).count
 		
 		var board2: Set<Coord> = []
 		for (left, right) in targetYLinePairs {
@@ -136,25 +67,25 @@ public struct Day15: Challenge {
 			})
 		
 		let boundingBox = self.targetY
-		let concurrentQueue = DispatchQueue(label: "com.queue.Concurrent", qos: .userInteractive, attributes: .concurrent)
+//		let concurrentQueue = DispatchQueue(label: "com.queue.Concurrent", qos: .userInteractive, attributes: .concurrent)
 		var result: String = ""
 		for targetY in 0...boundingBox {
 //			concurrentQueue.async {
-//				print("Trying y=\(targetY)")
-				var targetYLinePairs: [(Coord, Coord)] = []
-				for coord in coords {
-					let sensorCoord = coord.0
-					let beaconCoord = coord.1
-					let manhattanDistance = abs(sensorCoord.x - beaconCoord.x) + abs(sensorCoord.y - beaconCoord.y)
-					let distanceToTargetY = abs(targetY - sensorCoord.y)
-					if distanceToTargetY > manhattanDistance {
-						continue
-					}
-					let left = Coord(x: sensorCoord.x + (-1 * (manhattanDistance - distanceToTargetY)), y: targetY)
-					let right = Coord(x: sensorCoord.x + (1 * (manhattanDistance - distanceToTargetY)), y: targetY)
-					targetYLinePairs.append((left, right))
+//							print("Trying y=\(targetY)")
+			var targetYLinePairs: [(Coord, Coord)] = []
+			for coord in coords {
+				let sensorCoord = coord.0
+				let beaconCoord = coord.1
+				let manhattanDistance = abs(sensorCoord.x - beaconCoord.x) + abs(sensorCoord.y - beaconCoord.y)
+				let distanceToTargetY = abs(targetY - sensorCoord.y)
+				if distanceToTargetY > manhattanDistance {
+					continue
 				}
-				
+				let left = Coord(x: sensorCoord.x + (-1 * (manhattanDistance - distanceToTargetY)), y: targetY)
+				let right = Coord(x: sensorCoord.x + (1 * (manhattanDistance - distanceToTargetY)), y: targetY)
+				targetYLinePairs.append((left, right))
+			}
+			
 			targetYLinePairs.sort(by: { $0.0.x < $1.0.x })
 			var leftX: Int = 0
 			var rightX: Int = 0
@@ -173,21 +104,6 @@ public struct Day15: Challenge {
 					}
 				}
 			}
-				
-//				var board2: Set<Coord> = []
-//				for (left, right) in targetYLinePairs {
-//					let newLeftX = left.x < 0 ? 0 : left.x
-//					let newRightX = right.x > boundingBox ? boundingBox : right.x
-//					if newLeftX > newRightX {
-//						continue
-//					}
-//					board2.formUnion((newLeftX...newRightX).map({ Coord(x: $0, y: targetY) }))
-//				}
-//	//			board2.subtract(coords.map({ $0.1 }))
-//				if board2.count < boundingBox + 1 {
-//					let coord = Set((0...boundingBox).map({ Coord(x: $0, y: targetY) })).subtracting(board2).first!
-//					result = "\(coord.x * 4_000_000 + coord.y)"
-//				}
 //			}
 		}
 		
